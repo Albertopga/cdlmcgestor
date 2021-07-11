2864704ed8f20fed8bee77c13803cc36c89bc1f7
Alberto <albertopga@gmail.com>
2021-07-11 10:43:31 +0200

    edit userName

--- src/pages/Settings/Settings.js
+++ src/pages/Settings/Settings.js
@@ -1,17 +1,25 @@
 import React, { useState } from 'react'
 import UpdateAvatar from '../../components/Settings/UpdateAvatar';
+import UserName from '../../components/Settings/UserName';
+import BasicModal from '../../components/Modals/BasicModal';
 
 import './style.scss'
 
 export default function Settings(props) {
   const { user, setReloadApp } = props;
-
+  const [showModal, setShowModal] = useState(false);
+  const [titleModal, setTitleModal] = useState('');
+  const [contentModal, setContentModal] = useState(null);
   return (
     <div className='settings'>
       <h1>Editar usuario</h1>
       <div className='avatar-name'>
-        <UpdateAvatar user={user} setReloadApp={setReloadApp}/>        
+        <UpdateAvatar user={user} setReloadApp={setReloadApp}/>
+        <UserName user={user} setShowModal={setShowModal} setTitleModal={setTitleModal} setContentModal={setContentModal} setReloadApp={setReloadApp}/>
       </div>
+      <BasicModal show={showModal} setShow={setShowModal} title={titleModal} size='tiny'>
+        {contentModal}
+      </BasicModal>
     </div>
   )
 }
